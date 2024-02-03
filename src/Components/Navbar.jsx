624ad7ed704983789d3b4cc98585e4935e27a961
Anks <dubeyankur2833@gmail.com>
2024-02-03 23:47:22 +0530

    second

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
 
             <div className='nleft flex items-center'>
 
-                <img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="no img" />
+                <h1 className='font-bold underline -rotate-12 text-[1.2vw]'>reFokus</h1>
 
                 <div className="links flex gap-14 ml-20">
                     {["Home", "Work", "Culture", "", "News"].map(((elem, index) =>
